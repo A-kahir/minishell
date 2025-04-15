c17928734da12a40e644bfb6da247de7de705ffb
A-kahir <aymanekahir036@gmail.com>
2025-04-15 19:42:49 +0100

    start

---
+++ Makefile
@@ -0,0 +1,4 @@
+CC = gcc
+CFLAGS = -Wall -Werror -Wextra
+
+SRC = execution/exec.c execution/redirection.c 
--- minishell.h
+++ minishell.h
@@ -1,1 +1,17 @@
+
+
+
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct s_cmd
+{
+    char **args;
+    char *infile;
+    char *outfile;
+    int append;
+    int is_builtin;
+    struct s_command *next;
+} t_cmd;
+
