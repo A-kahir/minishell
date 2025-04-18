be507623474eee43435c031bff3bbb0e00065bf7
A-kahir <aymanekahir036@gmail.com>
2025-04-18 16:28:52 +0100

    akahir start export

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CFLAGS = -Wall -Wextra -Werror
 NAME = minishell
 
 SRC = execution/exec.c execution/redirection.c utils_func/ft_close_fd.c execution/min.c \
-	builtins/ft_echo.c builtins/ft_cd.c 
+	builtins/ft_echo.c builtins/ft_cd.c builtins/ft_pwd.c builtins/ft_export.c 
 
 OBJS = $(SRCS:.c=.o)
 
--- builtins/ft_export.c
+++ builtins/ft_export.c
@@ -1,39 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_pwd.c                                           :+:      :+:    :+:   */
+/*   ft_export.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akahir <akahir@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/17 22:30:02 by akahir            #+#    #+#             */
-/*   Updated: 2025/04/18 15:46:41 by akahir           ###   ########.fr       */
+/*   Created: 2025/04/18 15:54:22 by akahir            #+#    #+#             */
+/*   Updated: 2025/04/18 16:21:31 by akahir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void ft_pwd(char **str)
+void ft_export(char **str)
 {
-	char *buff;
-    int i;
-    int j;
-
-	i = 1;
-	if (str[i + 1] != NULL)
-	{
-		write(1, "pwd: too many arguments\n", 24);
-		return;
-	}
-	j = 0;
-	buff = getcwd(NULL, 0);
-	if (str[i] && ft_strncmp(str[i], "pwd", 3) == 0)
-	{
-		if (buff != NULL)
-		{
-			printf("%s\n", buff);
-			free(buff);
-		}
-		else
-			perror("pwd failed");
-	}
+    
 }
